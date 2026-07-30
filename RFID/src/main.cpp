52316272decb2827a3434488afe2820fc754218a
DisenioTiempoReal <163143277+DisenioTiempoReal@users.noreply.github.com>
2026-07-30 19:52:06 -0300

    version funcional

--- RFID/src/main.cpp
+++ RFID/src/main.cpp
@@ -1,62 +1,74 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <MFRC522.h>
 
-TaskHandle_t tareaLEDHandle;
-TaskHandle_t tareaMemoriaHandle;
+// Pines del RC522
+#define SS_PIN   21
+#define RST_PIN  22
 
-const int LED_BUILTIN = 2;
-
-
-void tareaLED(void *pvParameters)
-{
-    pinMode(LED_BUILTIN, OUTPUT);
-
-    while (true)
-    {
-        digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-        Serial.println("Tarea LED ejecutando");
-        vTaskDelay(pdMS_TO_TICKS(500));
-    }
-}
-
-void tareaMemoria(void *pvParameters)
-{
-    while (true)
-    {
-        char bufferGigante[2000];
-
-        for (int i = 0; i < sizeof(bufferGigante); i++)
-        {
-            bufferGigante[i] = i;
-        }
-
-        Serial.println("Tarea memoria ejecutando");
-
-        vTaskDelay(pdMS_TO_TICKS(1000));
-    }
-}
-
+MFRC522 rfid(SS_PIN, RST_PIN);
 
 void setup()
 {
     Serial.begin(115200);
 
-    xTaskCreate(
-        tareaLED,
-        "LED",
-        2048,
-        NULL,
-        1,
-        &tareaLEDHandle);
+    SPI.begin();             // SCK=18, MISO=19, MOSI=23 (ESP32)
+    rfid.PCD_Init();
 
-    xTaskCreate(
-        tareaMemoria,
-        "MEMORIA",
-        512,
-        NULL,
-        1,
-        &tareaMemoriaHandle);
+    Serial.println();
+    Serial.println("================================");
+    Serial.println("Lector RFID RC522");
+    Serial.println("Acerque una tarjeta...");
+    Serial.println("================================");
 }
 
 void loop()
 {
+    // ¿Hay una tarjeta presente?
+    if (!rfid.PICC_IsNewCardPresent())
+        return;
+
+    // ¿Se pudo leer?
+    if (!rfid.PICC_ReadCardSerial())
+        return;
+
+    Serial.print("UID: ");
+
+    for (byte i = 0; i < rfid.uid.size; i++)
+    {
+        if (rfid.uid.uidByte[i] < 0x10)
+            Serial.print("0");
+
+        Serial.print(rfid.uid.uidByte[i], HEX);
+        Serial.print(" ");
+    }
+
+    Serial.println();
+
+    // También en decimal
+    Serial.print("UID Decimal: ");
+
+    for (byte i = 0; i < rfid.uid.size; i++)
+    {
+        Serial.print(rfid.uid.uidByte[i]);
+
+        if (i < rfid.uid.size - 1)
+            Serial.print("-");
+    }
+
+    Serial.println();
+
+    // Tipo de tarjeta
+    MFRC522::PICC_Type type = rfid.PICC_GetType(rfid.uid.sak);
+
+    Serial.print("Tipo: ");
+    Serial.println(rfid.PICC_GetTypeName(type));
+
+    Serial.println("---------------------------");
+
+    // Finalizar comunicación
+    rfid.PICC_HaltA();
+    rfid.PCD_StopCrypto1();
+
+    delay(500);
 }
